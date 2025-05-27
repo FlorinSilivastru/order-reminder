43cda942bd915130ae13ab0abba55ec7375862a3
FlorinSilivastru <florinsilivastru@rapidazure.com>
2025-05-27 19:43:15 +0300

    add some data on the SendReminderMessage

--- NotificationService/NotificationService.Application/Consummers/Messages/SendReminderMessage.cs
+++ NotificationService/NotificationService.Application/Consummers/Messages/SendReminderMessage.cs
@@ -1,6 +1,14 @@
 ﻿namespace NotificationService.Application.Consummers.Messages;
 
-public class SendReminderMessage
+public record class SendReminderMessage
 {
+    public Guid OrderId { get; init; }
 
+    public DateTime OrderDate { get; init; }
+
+    public string OrderNumber { get; init; }
+
+    public decimal OrderAmount { get; init; }
+
+    public string EmailAddress { get; init; }
 }

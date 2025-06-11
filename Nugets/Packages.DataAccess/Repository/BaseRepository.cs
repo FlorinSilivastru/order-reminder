bacfa7eaaa098d57dc60218ee22834b22b6982aa
FlorinSilivastru <florinsilivastru@rapidazure.com>
2025-06-11 13:31:12 +0300

    generic repo added

---
+++ Nugets/Packages.DataAccess/Repository/BaseRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Packages.DataAccess.Repository.Contracts;
+
+namespace Packages.DataAccess.Repository;
+
+public class BaseRepository<TModel> : IBaseRepository<TModel>
+    where TModel : class
+{
+    protected readonly DbContext Context;
+
+    protected readonly DbSet<TModel> DbSet;
+
+    public BaseRepository(DbContext dbContext)
+    {
+        this.Context = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        this.DbSet = dbContext.Set<TModel>();
+    }
+
+    public virtual async Task AddAsync(TModel entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        await this.DbSet.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+    }
+
+    public virtual async Task<TModel?> GetByIdAsync(params object[] keyValues)
+    {
+        return await this.DbSet.FindAsync(keyValues);
+    }
+
+    public virtual async Task<List<TModel>> GetAllAsync()
+    {
+        return await this.DbSet.ToListAsync();
+    }
+
+    public virtual async Task UpdateAsync(TModel entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        this.DbSet.Update(entity);
+        await this.Context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TModel entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        this.DbSet.Remove(entity);
+        await this.Context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteByIdAsync(params object[] keyValues)
+    {
+        var entity = await this.GetByIdAsync(keyValues);
+        if (entity != null)
+        {
+            this.DbSet.Remove(entity);
+            await this.Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Nugets/Packages.DataAccess/Repository/Contracts/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Packages.DataAccess.Repository.Contracts
+{
+    public interface IBaseRepository<TModel> where TModel : class
+    {
+        Task AddAsync(TModel entity);
+        Task DeleteAsync(TModel entity);
+        Task DeleteByIdAsync(params object[] keyValues);
+        Task<List<TModel>> GetAllAsync();
+        Task<TModel?> GetByIdAsync(params object[] keyValues);
+        Task UpdateAsync(TModel entity);
+    }
+}
